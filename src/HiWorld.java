ec83e0a09e1c6c232b9753394a4fa1df5f6a6592
yyyy <611466660@qq.com>
2020-05-11 14:27:43 +0800

    test2

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -2,5 +2,6 @@ public class HiWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
         System.out.println("github is nb!");
+        System.out.println("i'm nb!");
     }
 }
